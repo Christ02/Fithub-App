0796cb7b45f344cffca8d60aa39b1e2996929f02
Christian <christianbarrios@ufm.edu>
2024-11-14 18:37:09 -0600

    arreglos en el docker-compose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,23 @@
-// config/db.js
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://root:Lolipop1!@localhost:27017/fithub_app', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      authSource: 'admin', 
-    });
-    console.log('Conectado a la base de datos MongoDB de Docker');
-  } catch (err) {
-    console.error('Error conectando a la base de datos:', err);
-    process.exit(1); 
+const connectDB = async (retries = 5, delay = 5000) => {
+  while (retries) {
+    try {
+      await mongoose.connect('mongodb://root:123a@mongo:27017/fithub', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        authSource: 'admin', // Usa la base de datos 'admin' para la autenticación
+      });
+      console.log('Conectado a la base de datos MongoDB de Docker');
+      break;
+    } catch (err) {
+      console.error('Error conectando a la base de datos:', err);
+      retries -= 1;
+      console.log(`Reintentando conexión. Intentos restantes: ${retries}`);
+      await new Promise(res => setTimeout(res, delay));
+    }
   }
+  if (!retries) process.exit(1); // Si no puede conectar después de varios intentos, se detiene
 };
 
 module.exports = connectDB;
